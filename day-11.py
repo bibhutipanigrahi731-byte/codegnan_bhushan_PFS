74a8cd5feb0ce12bcde4056b9c630127a9c7c08f
bibhutipanigrahi731-byte <bibhutipanigrahi731@gmail.com>
2026-07-02 00:31:43 -0700

    Add files via upload

---
+++ day-11.py
@@ -0,0 +1,82 @@
+#day-11.py
+
+'''
+modules
+--------
+a modules in python is a file that contains python code suc as
+--->varables
+-functions
+-classon
+-statement
+
+
+def add(a,b):
+def sub(a,b):
+    return a-b
+
+
+
+import math 
+print(math.sqrt(25))
+
+print(math.factorial(10))
+print(math.pow(2,5))
+
+
+import math 
+print(math.sqrt(25))
+import math as m
+print(m.pow(2,5))
+
+
+
+import os
+os.remove("deo.txt")
+
+
+import sys
+print(sys.version)
+
+     
+
+import sys
+print(sys.version)
+
+print(sys.exit)
+print(sys.path)
+
+
+import random
+print(random.randint(1000,9999))
+
+
+from collections import counter
+data = ['a','b','c','d']
+counter = counter(data)
+print(counter)
+
+dd = defaultdict(int)
+dd['missing'] += 1
+print(dd['missing'])
+print(dd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
